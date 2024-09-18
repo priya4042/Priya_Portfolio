3ec7c66dd81804c778ce15fe9e056c3713feaad1
priya4042 <137480965+priya4042@users.noreply.github.com>
2024-09-18 08:17:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -38,16 +38,6 @@ document.addEventListener('DOMContentLoaded', function() {
   observer.observe(skillSection);
 });
 
-// document.addEventListener('DOMContentLoaded', function() {
-//   const hamburger = document.querySelector('.hamburger');
-//   const navbar = document.querySelector('.navbar');
-  
-//   hamburger.addEventListener('click', function() {
-//     navbar.classList.toggle('active');
-//   });
-// });
-
-
 document.addEventListener('DOMContentLoaded', function() {
   const hamburger = document.querySelector('.hamburger');
   const navbar = document.querySelector('.navbar');
